3cc8b091c8309787b871d66a0693c65601a088bb
Pavel Hodoval <pajah14@gmail.com>
2023-10-02 12:00:18 +0200

    fix: fixed Product typehints to prevent mapping errors

--- src/Dto/Product.php
+++ src/Dto/Product.php
@@ -6,20 +6,20 @@ namespace Phodoval\KauflandMarketplace\Dto;
 class Product {
     public function __construct(
         public int     $id_product,
-        public string  $title,
+        public string   $title,
 
         /**
-         * @var int[]
+         * @var string[]
          */
-        public array   $eans,
-        public int     $id_category,
-        public string  $main_picture,
-        public string  $manufacturer,
-        public string  $url,
-        public string  $real_mgb_article_number,
-        public int     $age_rating,
-        public bool    $is_valid,
-        public ?string $dangerous_goods_li_shipping,
-        public ?string $danger_label_9A,
+        public array    $eans,
+        public int      $id_category,
+        public string   $main_picture,
+        public string   $manufacturer,
+        public string   $url,
+        public int      $age_rating,
+        public bool     $is_valid,
+        public ?string  $dangerous_goods_li_shipping,
+        public ?string  $danger_label_9A,
+        public ?string  $real_mgb_article_number = null,
     ) {}
 }
